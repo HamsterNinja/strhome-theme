2b07f93185311f0559042b29e7a13ffc23e126d6
Andrey Eroshkin <andreroshkin@gmail.com>
2019-11-26 14:27:20 +0300

    news_posts

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ $context['posts'] = new Timber\PostQuery();
 $context['foo']   = 'bar';
 $templates        = array( 'index.twig' );
 if ( is_home() ) {
+
+	$args_news = array(
+		'post_type' => 'post',
+		'posts_per_page' => 3,
+	);            
+	$context['news_posts'] = Timber::get_posts( $args_news );
+
 	array_unshift( $templates, 'front-page.twig', 'home.twig' );
 }
 Timber::render( $templates, $context );
