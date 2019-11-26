775ca49dbae384f8ad7ff61067bd098e7047d524
Andrey Eroshkin <andreroshkin@gmail.com>
2019-11-26 10:16:26 +0300

    wp theme

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url : 'http://localhost/strhome/',
+    themeDir : '/wp-content/themes/strhome-theme/',
+    fullPath : 'http://localhost/strhome/wp-content/themes/strhome-theme/',
+    ip  : "192.168.50.4"
+}
